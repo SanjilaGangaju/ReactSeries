d5b419ad6fb7394265823bff95ca7c6336a31e28
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-22 11:19:19 +0545

    handle form submit data and add data to the new state

--- src/projects/Todo/todo.jsx
+++ src/projects/Todo/todo.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+import '../Todo/Todo.css';
 export const Todo = ()=>{
+   const [inputValue, setInputValue] = useState("");
+    const [task, setTask] = useState([]);
+   const handleInputChange = (value)=>{
+    setInputValue(value);
+   };
+  
+    const handleFormSubmit= (event)=>{
+     event.preventDefault();
+     if (!inputValue) return ;
+
+     setTask((prevTask)=>{
+           [... prevTask, inputValue];
+     })
+    }
+
+    
     return (
         <>
         <section className="todo-container">
@@ -6,9 +24,9 @@ export const Todo = ()=>{
                 <h1>Todo List</h1>
             </header>
             <section className="form">
-                <form>
+                <form onSubmit ={handleFormSubmit}>
                     <div>
-                         <input type='text' autoComplete="off" className="todo-input"></input>
+                         <input type='text' autoComplete="off" className="todo-input" value={inputValue} onChange={(event)=>{handleInputChange(event.target.value)}}></input>
 
                     </div>
                     <div>
