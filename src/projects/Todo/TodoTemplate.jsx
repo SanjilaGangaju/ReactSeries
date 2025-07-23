2ca1d72eb50eb7390e1619b397287a6d07468014
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-23 20:49:07 +0545

    todo input form complete

--- src/projects/Todo/TodoTemplate.jsx
+++ src/projects/Todo/TodoTemplate.jsx
@@ -2,7 +2,7 @@ export const TodoTemplate= ({children}) =>{
     return (
         <>
         <section className="todo-template">
-            <div class="todo-container">
+            <div className="todo-container">
                 <h1>Todo App</h1>
                 {children}
             </div>
--- src/projects/Todo/todoForm.jsx
+++ src/projects/Todo/todoForm.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 export const TodoForm = ()=>{
+    const [inputValue, setInputValue] = useState({
+        task: ""});
+    const {task} = inputValue;
+    const handleInputValue = (e)=>{
+     
+        setInputValue({...inputValue, task: e.target.value});
+    }
+    const handleSubmit = (e)=>{
+      e.preventDefault();
+      setInputValue({task: ""})
+    }
     return (
         <>
         <div className="todo-form-container">
-            <form>
-                <input name="task" type="text"></input>
+            <form onSubmit = {handleSubmit}>
+                <input name="task" type="text" value={task} autoComplete="off" onChange={handleInputValue}></input>
                 <button type="submit">Add Todo</button>
             </form>
             
