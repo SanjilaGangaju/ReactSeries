78cfb3e166912aeb1fa61e1be209328636c2a6ef
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-24 15:39:17 +0545

    create todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,8 @@
+import { TodoList } from "./projects/Todo/TodoList";
 
-// import {Greeting} from './components/Greeting';
-// import InfoCard from './components/InfoCard';
-// import Button from './components/button';
-// import ClickButton from './components/clickButton';
-// import { DerivedState } from './components/DerivedState';
-// import { LiftingState } from './components/liftState';
-// import ToggleSwitchButton from './projects/ToggleSwitchButton/toggleSwitchbutton';
-// import {Todo} from './projects/Todo/todo';
-// import {InputStates} from './components/inputStates';
-import { useState } from 'react';
-import { TodoForm } from './projects/Todo/todoForm';
-import { TodoLists } from './projects/Todo/TodoLists';
-import { TodoTemplate } from './projects/Todo/TodoTemplate';
 function App(){
-  // const isLoggedIn = true;
-  // const user = {name: 'Sanjila', age: 22};
-//   return (
-//     <>
-//      {/* <Greeting isLoggedIn ={isLoggedIn} user={user}></Greeting>
-//      <InfoCard title="React Starter" description="This is a starter project to learn React fundamentals"></InfoCard>
-//      <Button label="click me"></Button>
-//      <ClickButton></ClickButton>
-//      <DerivedState></DerivedState>
-//     
-//     </>
-//    return (
-//     <>
-//     {/* <ToggleSwitchButton></ToggleSwitchButton> */}
-//     {/* <section><Todo></Todo></section> */}
-//     <InputStates></InputStates>
-//     </>
-//    )
-const [todos, setTodos] = useState([]);
-
-const addTodo = (text) =>{
-  const newTodo = {text:text, completed:false};
-  setTodos(newTodo);
-  console.log(todos);
+  return (
+    <TodoList></TodoList>
+  )
 }
-       return (
-        <TodoTemplate>
-          <TodoForm addTodo={addTodo}></TodoForm>
-          <TodoLists></TodoLists>
-        </TodoTemplate>
-        
-       )
-// //   );
- }
 export default App;
---
+++ src/projects/Todo/TodoList.jsx
@@ -0,0 +1,85 @@
+import '../Todo/todo.css';
+import { v4 as uuidv4 } from 'uuid';
+import {useState} from 'react';
+export const TodoList = ()=>{
+  const [inputValue, setInputValue] = useState("");
+  const [task, setTask] = useState([])
+
+  
+  const handleInputValue = (e) =>{
+       setInputValue(e.target.value);
+      
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    //empty input validaton
+    if(!inputValue) return;
+
+     //duplicate validation
+    const duplicate = task.find((currTask)=>
+        currTask.todo === inputValue)
+    if (duplicate) {setInputValue("");
+     return};
+        
+    
+    const newTask = {todo: inputValue, checked:false, index: uuidv4()};
+    
+   
+    
+    setTask(prevTask=>[...prevTask, newTask]);
+
+    
+    setInputValue("");
+  }
+  const handleCheck=(value)=>{
+   
+    const updatedTask = task.map((todoItem)=>{
+       if (todoItem.index === value.index){
+          return {...todoItem, checked:!todoItem.checked};
+          
+       }
+       else{ return todoItem};
+      
+    })
+  setTask(updatedTask);
+
+  }
+  const handleDelete=(value)=>{
+       task.filter((currTask)=>{
+        
+       })
+  }
+  return (
+    <>
+  <h1>Todo App</h1>
+  <div className="todo-form--container">
+   <form onSubmit={handleSubmit}>
+    <input type="text" value={inputValue} onChange={handleInputValue}></input>
+    <button type="submit">Add Todo</button>
+   </form>
+  </div>
+  <div className="todo-lists">
+    <ul>
+      {task.map((currTask)=>{
+        const {index, checked, todo}  = currTask;
+        
+           return (
+           
+              <li key ={index}>
+                
+                 <span className="todo-name" style={{ textDecoration: checked ? "line-through" : "none" }}>{todo}</span>
+                  <button onClick={()=>handleCheck(currTask)}>Check</button>
+                  <button onClick={()=>handleDelete(currTask)}>Delete</button>
+
+                
+              </li>
+           );
+
+      })}
+    </ul>
+  </div>
+  </>
+);
+
+}
