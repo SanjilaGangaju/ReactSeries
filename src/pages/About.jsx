efa040a3043637a54203d3dfda72ae7e037cb661
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-30 08:46:09 +0545

    use Navigate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 const About = () => {
+
+  const navigate = useNavigate();
+  const Home =()=>{
+    navigate('/')
+  }
+  const Previous = ()=>{
+    navigate(-1)
+  }
   return (
-    <div>About</div>
+    <div>
+      <button onClick={Home}>Go to Home</button>
+      <button onClick={Previous}>Previous</button>
+    </div>
   )
 }
 
